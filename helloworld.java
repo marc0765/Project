45f830801bbe8c07f95c2f0046790fea6fc408e5
marc0765 <124487023+marc0765@users.noreply.github.com>
2023-02-06 20:01:40 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,6 @@
 class helloworld{
-   public static void main(String[] args) {
-    System.out.print("helloworld");
+   public static void main(String[] args)
+   {
+      System.out.print("hello world");
    }
 }
